3e38b4f49ebea691fc84a749663e71e916f82cfa
James Drever <james@careful.digital>
2023-03-14 08:09:12 +0000

    sorting missing .map files

--- src/webpack.mix.js
+++ src/webpack.mix.js
@@ -11,9 +11,10 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.copy(['../src/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js'], 'public/js/bootstrap.js')
+mix.copy(['../src/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js','../src/node_modules/bootstrap/dist/js/bootstrap.bundle.min.js.map'],'public/js/')
     .copy(['resources/js/site-name-autocomplete.js','resources/js/species-name-autocomplete.js','resources/js/update-dataset.js'], 'public/js/')
-    .combine(['resources/js/BasicMap.js', '../src/node_modules/leaflet/dist/leaflet.js','resources/js/Leaflet.MetricGrid.js', '../src/node_modules/proj4/dist/proj4.js','../src/node_modules/wicket/wicket.js', '../src/node_modules/brc-atlas-bigr/dist/bigr.min.umd.js', '../src/node_modules/d3/dist/d3.min.js'], 'public/js/mapping.js')
+    .combine(['resources/js/BasicMap.js', '../src/node_modules/leaflet/dist/leaflet.js','resources/js/Leaflet.MetricGrid.js', '../src/node_modules/proj4/dist/proj4.js','../src/node_modules/wicket/wicket.js', '../src/node_modules/brc-atlas-bigr/dist/bigr.min.umd.js', '../src/node_modules/d3/dist/d3.min.js'], 'public/js/mapping.js').sourceMaps()
+    .copy(['../src/node_modules/leaflet/dist/leaflet.js.map'], 'public/js/')
     .sass('resources/scss/bootstrap.scss', 'public/css/bootstrap.css')
     .copy(['resources/css/app.css','resources/css/shropshire-style.css', 'resources/css/enhancements.css'], 'public/css/')
     .css('../src/node_modules/leaflet/dist/leaflet.css','public/css/leaflet.css');
